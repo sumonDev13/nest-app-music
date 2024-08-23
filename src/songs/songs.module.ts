7d2bf385d89327384b0239429fd3466a383ca1ac
sumonDev13 <sumonmondal1530765@gmail.com>
2024-08-23 21:15:20 +0600

    class type provider

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -4,6 +4,12 @@ import { SongsService } from './songs.service';
 
 @Module({
   controllers: [SongsController],
-  providers: [SongsService],
+  providers: [SongsService
+    //@option 2 :
+    // {
+    //   provide:SongsService,
+    //   useClass:SongsService,
+    // }
+  ],
 })
 export class SongsModule {}
